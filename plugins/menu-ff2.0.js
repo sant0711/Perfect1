6a9b6638d96658b3148ae3e76baecf622489ac81
sant0711 <santx0711@gmail.com>
2024-12-18 23:49:56 -0500

    Delete plugins/menu-ff2.0.js

--- plugins/menu-ff2.0.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, text, command }) => {
-let id = text ? text : m.chat  
-let pp = 'https://telegra.ph/file/5ab1ca8bf65c1ddb36c20.mp4'
-await conn.sendMessage(m.chat, { video: { url: pp }, gifPlayback: true, caption: '*Adios a todos, el Bot se despide! 𝙎𝘼𝙉𝙏 𝘽𝙊𝙏 - 𝙈𝘿 👑')
-handler.help = ['menuff']
-handler.tags = ['freefire' ,'main'] 
-handler.command = ['menuff22', 'menufreefire']
-
-export default handler
